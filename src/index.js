09af91bd2cb8efeebbe7d31204dc3de4a6728451
boue <boue.germain@gmail.com>
2019-03-03 16:47:23 -0800

    Connect Data-Layer to View-Layer with react-apollo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "apollo-client";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
-import registerServiceWorker from './registerServiceWorker';
-import App from './App';
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./App";
 
-import './style.css';
+import "./style.css";
+
+const GITHUB_BASE_URL = "https://api.github.com/graphql";
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+    }`
+  }
+});
+
+// create the cache as the place where data is managed in Apollo client
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 registerServiceWorker();
