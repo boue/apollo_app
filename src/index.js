a0b1538a03819e0f56e3d787035fb01140422a83
boue <boue.germain@gmail.com>
2019-03-03 16:32:00 -0800

    The ApolloClient class is used to create the client instance, and the HttpLink and InMemoryCache are used for its mandatory configurations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
